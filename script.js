2cc650d767fbceb4cf22d31de3b805153a5cedb2
msgozde0 <msgozde66@gmail.com>
2025-05-18 09:58:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+//I created 3 variables 
+let studentName = "Mustafa"
+let studentAge = 25;
+let isEnrolled = true;
+
+
+console.log(studentName, studentAge, isEnrolled); 
+
+let carBrand = "Ferrari"
+let carHP = 890;
+let isWorking = true; 
+let carColors = ["blue", "white", "red"] //Array, a collection of values 
+console.log (carBrand);
+console.log (carHP);
+console.log (isWorking);
+console.log (carColors[2]);
+
+//objects, an entity with properties
+let car = {
+    brand: "Porsche",
+    model: "Cayenne"
+};
+console.log (car.brand);
+console.log (car.model);
+
+
+let a = 15;
+let b = 3; 
+
+//arithmetic operators
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("a + b = ", a+b);
+console.log("a - b = ", a-b);
+console.log("a * b = ", a*b);
+console.log("a / b = ", a/b);
+
+//comparison operators
+console.log("a > b : ", a>b);
+console.log("a < b : ", a<b);
+console.log("a === b : ", a===b);
+console.log("a !== b : ", a!==b);
+
+//logical operators
+let c = true;
+let d = false;
+console.log(c && d);
+console.log(c || d);
+console.log(!c);
